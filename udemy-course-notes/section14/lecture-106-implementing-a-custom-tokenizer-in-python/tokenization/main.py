12d8811c49cdce5a4b4ad8386854cee16789660a
Divyanshu Sahu <257divyanshu@gmail.com>
2026-06-16 18:08:31 +0530

    notes update for 16/06/26 - learnt about vector embeddings

--- udemy-course-notes/section14/lecture-106-implementing-a-custom-tokenizer-in-python/tokenization/main.py
+++ udemy-course-notes/section14/lecture-106-implementing-a-custom-tokenizer-in-python/tokenization/main.py
@@ -9,6 +9,6 @@ tokens = enc.encode(textToEncode) # encode the text
 print("tokens:",tokens) # logs the tokens
 # OUTPUT : tokens [39, 644, 105025, 63682, 11674, 1962, 268, 0]
 
-decodedOutput = enc.decode([39, 644, 105025, 63682, 11674, 1962, 268, 0])
+decodedOutput = enc.decode([39, 644, 105025, 63682, 11674, 1962, 268, 0]) # decode the encoded text
 print("decodedOutput:",decodedOutput) # logs the decodedOutput
 # OUTPUT : decodedOutput: Hare Krishna Dear Devotees!
